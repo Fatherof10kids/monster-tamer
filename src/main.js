0d59325d04d96b4407a41ab616d5dfd07fc8a621
DUNG MANH TRAN/LGEDV FACTORY SW TEAM(dung.tran@lge.com) <dung.tran@lge.com>
2024-07-25 07:31:22 +0700

    Adjust the scene that can fit any devices screen

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import { SCENE_KEYS } from "./scenes/scene-keys.js";
 import { PreloadScene } from "./scenes/preload-scene.js";
 
 const game = new Phaser.Game({
+    type: Phaser.CANVAS,
+    pixelArt: false,
     parent: 'game-container',
+    backgroundColor: "#000000",
+    scale: {
+        parent: 'game-container',
+        width: 1024,
+        height: 576,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
 });
 
 game.scene.add(SCENE_KEYS.PRELOAD_SCENE,PreloadScene);
